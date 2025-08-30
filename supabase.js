826c575ee27fc7a5ba545903b61089b6d201e80c
linnuha <73259210+linnuha@users.noreply.github.com>
2025-08-30 15:15:05 +0700

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,10 @@
+<!-- supabase.js -->
+<script type="module">
+  import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+  // Ganti dengan URL & anon key project Supabase kamu
+  export const supabase = createClient(
+    "hhttps://qlhnigssqyrajriufwrp.supabase.co", 
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFsaG5pZ3NzcXlyYWpyaXVmd3JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5Nzk3MTksImV4cCI6MjA2NzU1NTcxOX0.0V-1R0AlEaWsN_ftCIty7Ckbka8RNHuPvwu_wPWKTcA"
+  );
+</script>
